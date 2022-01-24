da838b02b7430888337ae4ab8e7ee5a15725b126
= <=>
2022-01-24 15:49:01 +0000

    Initial Commit

---
+++ bank system/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bank system
+//
+//  Created by jack on 24/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
