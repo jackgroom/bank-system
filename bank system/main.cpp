2a47267ef7644396d62ea223b5fd1cfeb78ca72f
= <=>
2022-01-24 16:07:37 +0000

    created user struct

--- bank system/main.cpp
+++ bank system/main.cpp
@@ -6,9 +6,28 @@
 //
 
 #include <iostream>
+#include <string>
+
+struct User {
+private:
+    int account_number {};
+    int account_balance {};
+    
+    std::string account_name;
+    int account_pin;
+    
+public:
+    User(int newAccountNumber) : account_number{ newAccountNumber }, account_balance{ 0 } {}
+    
+    int get_balance() { return account_balance; };
+    void deposit(int x) { account_balance += x; };
+    void withdraw(int x) { account_balance -= x; };
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    User* newUser = new User(1234);
+    std::cout << newUser->get_balance() << "\n";
+    
     return 0;
 }
